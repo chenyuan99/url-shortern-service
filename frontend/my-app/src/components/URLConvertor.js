8378b222132d7898f3e39ff3f1798b6e07fdc999
Yuan Chen <18861720589@163.com>
2022-12-06 23:42:42 -0600

    feat(fontend):connect front end and back end with

--- frontend/my-app/src/components/URLConvertor.js
+++ frontend/my-app/src/components/URLConvertor.js
@@ -1,11 +1,13 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import {useState} from "react";
+import {Table} from "react-bootstrap";
 
 function URLConvertor() {
 
     const [longURL, setLongURL] = useState("");
     const [shortURL, setShortURL] = useState("");
+    const [expiredDate, setExpiredDate] = useState("");
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -15,9 +17,14 @@ function URLConvertor() {
         const long = event.target.elements.long
         const short = event.target.elements.short
 
+
         var formdata = new FormData(event.target);
 
-        formdata.append("long_url", short.value);
+        if (long.value) {
+            formdata.append("long_url", long.value);
+        } else {
+            formdata.append("short_url", short.value);
+        }
 
         const data = Object.fromEntries(formdata.entries());
         console.dir(data);
@@ -29,47 +36,81 @@ function URLConvertor() {
         };
 
 
-        // fetch("http://127.0.0.1:8000/api/shorten", requestOptions)
-        //     .then(response => response.text())
-        //     .then(result => console.log(result))
-        //     .catch(error => console.log('error', error));
-
         // GET request using fetch with error handling
-        fetch("/api/shorten", requestOptions)
-            .then(response => {
-                // console.log(response.text())
-                response.text().then(result => {
-                    console.log(result)
-                    setLongURL(result)
-                    // console.log(result)
+        if (long.value) {
+            fetch("/api/shorten", requestOptions)
+                .then(response => {
+                    response.json().then(result => {
+                        console.log(result["shorten_url"])
+                        console.log(result["expired date"])
+                        setShortURL(result["shorten_url"])
+                        setExpiredDate((result["expired date"]))
+
+                    })
+
+
                 })
+                .catch(error => {
+                    console.log(error)
+                    console.error('There was an error!', error);
+                });
+        } else {
+            // GET request using fetch with error handling
+            fetch("/api/resolve", requestOptions)
+                .then(response => {
+                    response.json().then(result => {
+                        console.log(result["resolved_url"])
+                        setLongURL(result["resolved_url"])
+
+                    })
 
 
-            })
-            .catch(error => {
-                console.log(error)
-                console.error('There was an error!', error);
-            });
+                })
+                .catch(error => {
+                    console.log(error)
+                    console.error('There was an error!', error);
+                });
+        }
     }
 
 
     return (
         <>
             <Form onSubmit={handleSubmit}>
+
+                <Form.Group className="mb-3" controlId="formBasicLongURL">
+                    <Form.Label>Long URL</Form.Label>
+                    <Form.Control type="url" placeholder="Long URL" name="long"/>
+                </Form.Group>
+
                 <Form.Group className="mb-3" controlId="formBasicShortURL">
                     <Form.Label>Short URL</Form.Label>
                     <Form.Control type="url" placeholder="Short URL" name="short"/>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicLongURL">
-                    <Form.Label>Long URL</Form.Label>
-                    <Form.Control type="url" placeholder="Long URL"  name="long"/>
-                </Form.Group>
+
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
             </Form>
-            <h1>{longURL}</h1>
+            <Table>
+
+                <thead>
+                <tr>
+                    <th>Long URL</th>
+                    <th>Short URL</th>
+                    <th>Expired Date</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                <tr>
+                    <td>{longURL}</td>
+                    <td>{shortURL}</td>
+                    <td>{expiredDate}</td>
+                </tr>
+                </tbody>
+            </Table>
         </>
 
     );
